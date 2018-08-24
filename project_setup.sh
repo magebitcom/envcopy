6cadde80560d427334abba4b340681796101fdc9
root <vitalijs.volodenkovs@inbox.lv>
2018-08-24 19:49:44 +0300

    prefix temp

--- project_setup.sh
+++ project_setup.sh
@@ -13,10 +13,13 @@ echo "ERROR: Unknown project type."
 exit 1
 fi
 
-echo -n "Define DB name [default: staging]: "
+echo -n "Define DB name [example: staging]: "
 read mysql_db
+
+echo -n "Define table prefix if exist(OPTIONAL) [example: mage_]: "
+read table_prefix
 
 echo -n "Define AWS SSH username: "
 read aws_username
 #################################################################################
-./ansible-playbook -K -i localhost, playbooks/envcopy.yml -e project=$project -e project_type=$project_type -e aws_username=$aws_username -e mysql_db=$mysql_db
+./ansible-playbook -K -i localhost, playbooks/envcopy.yml -e project=$project -e project_type=$project_type -e aws_username=$aws_username -e mysql_db=$mysql_db -e table_prefix=$table_prefix
