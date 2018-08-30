9820fd58c31fea80346f3babe7620fb540cdfd56
root <vitalijs.volodenkovs@inbox.lv>
2018-08-30 16:30:47 +0300

    mysql_db, table_prefix

--- project_setup.sh
+++ project_setup.sh
@@ -2,13 +2,7 @@
 echo -n "Define project name without .stgin.com domain name [example: projectx]: "
 read project
 
-echo -n "Define DB name [example: staging]: "
-read mysql_db
-
-echo -n "Define table prefix if exist(OPTIONAL) [example: mage_]: "
-read table_prefix
-
 echo -n "Define AWS SSH username: "
 read aws_username
 #################################################################################
-./ansible-playbook -K -i localhost, playbooks/envcopy.yml -e project=$project -e project_type=$project_type -e aws_username=$aws_username -e mysql_db=$mysql_db -e table_prefix=$table_prefix
+./ansible-playbook -K -i localhost, playbooks/envcopy.yml -e project=$project -e aws_username=$aws_username
