0c178a6284d83f2e7bbe376e4fd7b1b647f66bbf
Vitalijs Volodenkovs <vitalijs.volodenkovs@leongaming.com>
2018-09-02 12:33:49 +0300

    scp

--- project_setup.sh
+++ project_setup.sh
@@ -4,5 +4,8 @@ read project
 
 echo -n "Define AWS SSH username: "
 read aws_username
+
+echo -n "Define AWS SSH private_key[example: /home/username/.ssh/id_rsa]: "
+read aws_private_key_path
 #################################################################################
-./ansible-playbook -K -i localhost, playbooks/envcopy.yml -e project=$project -e aws_username=$aws_username
+./ansible-playbook -K -i localhost --private-key=$aws_private_key_path, playbooks/envcopy.yml -e project=$project -e aws_username=$aws_username -e aws_private_key_path=$aws_private_key_path
