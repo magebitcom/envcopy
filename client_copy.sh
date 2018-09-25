dd327c530f8dfd54bc1b99088e26b8d1f77f65cb
root <vitalijs.volodenkovs@inbox.lv>
2018-09-25 16:54:46 +0300

    fix

--- client_copy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo -n "Define project name without .stgin.com domain name [example: projectx]: "
-read project
-
-echo -n "Define project_root [example: /var/www/projectx]: "
-read project_root
-
-#################################################################################
-./ansible-playbook -K -i localhost, playbooks/envcopy.yml -e project=$project -e project_root=$project_root
