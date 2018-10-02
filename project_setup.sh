e8619965620e9d4674ad3ec9635df01748c9e754
root <vitalijs.volodenkovs@inbox.lv>
2018-10-02 15:25:31 +0300

    ignore-media

--- project_setup.sh
+++ project_setup.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
+project_dump=$1
 echo -n "Define project name without .stgin.com domain name [example: projectx]: "
 read project
 
 #################################################################################
-./ansible-playbook -K -i localhost, playbooks/envcopy.yml -e project=$project
+if [[ "$project_dump" == "--ignore-media" ]] 
+then
+  ./ansible-playbook -K -i localhost, playbooks/envcopy.yml -e project=$project -e project_dump='false'
+else
+  ./ansible-playbook -K -i localhost, playbooks/envcopy.yml -e project=$project -e project_dump='true'
+fi
