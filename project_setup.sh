292fc8a904dc90dc8a1a7aebd4eba465ebb0ba68
root <vitalijs.volodenkovs@inbox.lv>
2018-10-03 12:06:12 +0300

    fixes

--- project_setup.sh
+++ project_setup.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
-media_dump=$1
-echo -n "Define project name without .stgin.com domain name [example: projectx]: "
-read project
-
+params=$1
 #################################################################################
 export ANSIBLE_HOST_KEY_CHECKING=False
-if [[ "$media_dump" == "--ignore-media" ]] 
+if [[ "$params" == "--ignore-media" ]] 
 then
+  echo -n "Define project name without .stgin.com domain name [example: projectx]: "
+  read project
   ./ansible-playbook -K -i localhost, playbooks/envcopy.yml -e project=$project -e media_dump='false'
+elif [[ "$params" == "--help" ]]
+then
+  echo 'Options:'
+  echo '  --ignore-media: Dump project without media'
 else
+  echo -n "Define project name without .stgin.com domain name [example: projectx]: "
+  read project
+  echo ' '
+  echo 'Use "./project_setup.sh --help" for more information about a given command.'
+  sleep 2
   ./ansible-playbook -K -i localhost, playbooks/envcopy.yml -e project=$project -e media_dump='true'
 fi
