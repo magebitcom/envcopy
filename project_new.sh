1c00d5174e66f75554eeb87d5477a8b78f93cea5
Vitalijs Volodenkovs <vitalijsvolodenkovs@VV.local>
2019-02-07 10:37:36 +0200

    fix

--- project_new.sh
+++ project_new.sh
@@ -2,7 +2,7 @@
 export ANSIBLE_HOST_KEY_CHECKING=False
 echo -n "Define project name without .stgin.com domain name [example: projectx]: "
 read project
-echo -n "Define project type [m1 or m2]: "
+echo -n "Define project type ['m1','m2','blank']: "
 read project_type
 echo -n "Define project type [5.6 or 7.0 or 7.1]: "
 read php_version
