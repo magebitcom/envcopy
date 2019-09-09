8172693aa5c2c3aae34c771837a45b567cd65775
Your Name <you@example.com>
2019-09-09 20:25:15 +0300

    vue project beta

---
+++ vue_project_setup.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+params=$1
+#################################################################################
+export ANSIBLE_HOST_KEY_CHECKING=False
+if [[ "$params" == "--ignore-media" ]] 
+then
+  echo -n "Define project name without .stgin.com domain name [example: projectx]: "
+  read project
+  ./ansible-playbook -K -i localhost, playbooks/envcopy.yml -e project=$project -e media_dump='false' -e vue_project='true'
+elif [[ "$params" == "--help" ]]
+then
+  echo 'Options:'
+  echo '  --ignore-media: Dump project without media'
+  echo '  --vue-project: Clone cue project as well'
+else
+  echo -n "Define project name without .stgin.com domain name [example: projectx]: "
+  read project
+  echo ' '
+  echo 'Use "./project_setup.sh --help" for more information about a given command.'
+  sleep 2
+  ./ansible-playbook -K -i localhost, playbooks/envcopy.yml -e project=$project -e media_dump='true' -e vue_project='true'
+fi
