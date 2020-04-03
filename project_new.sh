8d6b3b237bc2d61d585ef8a98d99bf3d788f6c05
Raimonds Vizulis <raimonds@magebit.com>
2020-04-03 21:04:45 +0300

    Resolve unmerged conflict

--- project_new.sh
+++ project_new.sh
@@ -2,11 +2,7 @@
 export ANSIBLE_HOST_KEY_CHECKING=False
 echo -n "Define project name without .stgin.com domain name [example: projectx]: "
 read project
-<<<<<<< HEAD
-echo -n "Define project type ['m1','m2','blank']: "
-=======
 echo -n "Define project type [m1 or m2 or blank]: "
->>>>>>> 3045ce94943985c0ff97b979e344043748978f72
 read project_type
 echo -n "Define project type [5.6 or 7.0 or 7.1]: "
 read php_version
