779063c3e6b662ccb48736bd284d2d3128a6840a
Your Name <you@example.com>
2021-06-07 11:29:47 +0300

    project_new.sh

--- project_new.sh
+++ project_new.sh
@@ -4,6 +4,8 @@ echo -n "Define project name without .stgin.com domain name [example: projectx]:
 read project
 echo -n "Define project type [m1 or m2 or blank]: "
 read project_type
-echo -n "Define project type [5.6 or 7.0 or 7.1]: "
+echo -n "Define project type [7.3 or 7.4 or 8.0]: "
 read php_version
-ansible-playbook -K -i localhost, playbooks/project_new.yml -e project=$project -e project_type=$project_type -e php_version=php$php_version-fpm
+echo -n "Define mysql version [5.7 or 8.0]: "
+read mysql_version
+ansible-playbook -K -i localhost, playbooks/project_new.yml -e project=$project -e project_type=$project_type -e php_version=php$php_version-fpm -e mysql_version=$mysql_version
