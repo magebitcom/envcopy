0af09fe46091cee8be4cbcecbb0037496e49e509
Your Name <you@example.com>
2021-06-08 10:42:17 +0300

    mysql_version rm

--- vue_project_setup.sh
+++ vue_project_setup.sh
@@ -6,17 +6,17 @@ if [[ "$params" == "--ignore-media" ]]
 then
   echo -n "Define project name without .stgin.com domain name [example: projectx]: "
   read project
-  ansible-playbook -K -i localhost, playbooks/envcopy.yml -e project=$project -e media_dump='false' -e vue_project='true' -e mysql_version=$mysql_version
+  ansible-playbook -K -i localhost, playbooks/envcopy.yml -e project=$project -e media_dump='false' -e vue_project='true'
 elif [[ "$params" == "--help" ]]
 then
   echo 'Options:'
   echo '  --ignore-media: Dump project without media'
-  echo '  --vue-project: Clone cue project as well'
+  echo '  --vue-project: Clone vue project as well'
 else
   echo -n "Define project name without .stgin.com domain name [example: projectx]: "
   read project
   echo ' '
   echo 'Use "./project_setup.sh --help" for more information about a given command.'
   sleep 2
-  ansible-playbook -K -i localhost, playbooks/envcopy.yml -e project=$project -e media_dump='true' -e vue_project='true' -e mysql_version=$mysql_version
+  ansible-playbook -K -i localhost, playbooks/envcopy.yml -e project=$project -e media_dump='true' -e vue_project='true'
 fi
