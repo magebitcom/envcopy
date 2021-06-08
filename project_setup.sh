a8ad2ff849a81b964bfebc60de7f0a01c9f1bb3b
Your Name <you@example.com>
2021-06-08 10:33:48 +0300

    mysql detect

--- project_setup.sh
+++ project_setup.sh
@@ -6,8 +6,6 @@ if [[ "$params" == "--ignore-media" ]]
 then
   echo -n "Define project name without .stgin.com domain name [example: projectx]: "
   read project
-  echo -n "Define mysql version [5.7 or 8.0]: "
-  read mysql_version
   ansible-playbook -K -i localhost, playbooks/envcopy.yml -e project=$project -e media_dump='false' -e mysql_version=$mysql_version
 elif [[ "$params" == "--help" ]]
 then
@@ -16,8 +14,6 @@ then
 else
   echo -n "Define project name without .stgin.com domain name [example: projectx]: "
   read project
-  echo -n "Define mysql version [5.7 or 8.0]: "
-  read mysql_version
   echo ' '
   echo 'Use "./project_setup.sh --help" for more information about a given command.'
   sleep 2
--- vue_project_setup.sh
+++ vue_project_setup.sh
@@ -6,8 +6,6 @@ if [[ "$params" == "--ignore-media" ]]
 then
   echo -n "Define project name without .stgin.com domain name [example: projectx]: "
   read project
-  echo -n "Define mysql version [5.7 or 8.0]: "
-  read mysql_version
   ansible-playbook -K -i localhost, playbooks/envcopy.yml -e project=$project -e media_dump='false' -e vue_project='true' -e mysql_version=$mysql_version
 elif [[ "$params" == "--help" ]]
 then
@@ -17,8 +15,6 @@ then
 else
   echo -n "Define project name without .stgin.com domain name [example: projectx]: "
   read project
-  echo -n "Define mysql version [5.7 or 8.0]: "
-  read mysql_version
   echo ' '
   echo 'Use "./project_setup.sh --help" for more information about a given command.'
   sleep 2
