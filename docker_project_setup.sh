357dd2edd1e2f66afecee90bf5ee3caea3c9b2f7
Vitalijs Volodenkovs <vitalijsvolodenkovs@VV.local>
2021-08-18 16:51:43 +0300

    docker test

---
+++ docker_project_setup.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+params=$1
+#################################################################################
+export ANSIBLE_HOST_KEY_CHECKING=False
+if [[ "$params" == "--ignore-media" ]] 
+then
+  echo -n "Define project name without .stgin.com domain name [example: projectx]: "
+  read project
+  ansible-playbook -K -i localhost, playbooks/docker_envcopy.yml -e project=$project -e media_dump='false' -e docker_project='true'
+elif [[ "$params" == "--help" ]]
+then
+  echo 'Options:'
+  echo '  --ignore-media: Dump project without media'
+  echo '  --docker-project: Clone docker project as well'
+else
+  echo -n "Define project name without .stgin.com domain name [example: projectx]: "
+  read project
+  echo ' '
+  echo 'Use "./project_setup.sh --help" for more information about a given command.'
+  sleep 2
+  ansible-playbook -K -i localhost, playbooks/docker_envcopy.yml -e project=$project -e media_dump='true'
+fi
