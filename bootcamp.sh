493e531ff19621f4302ba898a61bce5f8d1e9955
Rihards Ratke <rihards.ratke@magebit.com>
2022-07-26 16:22:13 +0300

    Bootcamp - added comment blocks

--- bootcamp.sh
+++ bootcamp.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+
+# Admin configs
 USER_NEW=magebit_admin
 PASS=1GM1v7wK
 
+# Setup PC
 snap install --classic code
 
 cp ./bootcamp/nginx/server.conf /etc/nginx/sites-enabled/
@@ -13,14 +16,16 @@ cp bootcamp/server/index.php /var/www/web/
 echo "127.0.0.1 web.local" >> /etc/hosts
 service nginx restart
 
+## Create new Admin user
 useradd -p $(openssl passwd -crypt $PASS) $USER_NEW
 usermod -aG sudo $USER_NEW
 
-USER=1
+USER_NEW=1
 PASS=1
 
-echo "alias sudo='echo "!!!! not allowed !!!!" '" >> ~/.bashrc
+echo "alias sudo='echo "not allowed" '" >> ~/.bashrc
 chmod 400 ~/.bashrc
 
-rm -rf .git/
+## delete passwords and change to reverse back
+rm -rf .git/*
 echo '' > ./bootcamp.sh
