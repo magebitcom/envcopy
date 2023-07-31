a2baa1a2240ddfc2b32ac8c56c26e93f771d9da7
Rihards Ratke <rihards.ratke@magebit.com>
2023-07-31 12:24:23 +0300

    Updated bootcamp php8.2 and simplified script

--- bootcamp.sh
+++ bootcamp.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-# Admin configs
-USER_NEW=magebit_admin
-PASS=1GM1v7wK
-
 # Setup PC
 snap install --classic code
 
@@ -16,16 +12,4 @@ cp -r bootcamp/server /var/www/web/
 echo "127.0.0.1 web.local" >> /etc/hosts
 service nginx restart
 
-## Create new Admin user
-useradd -p $(openssl passwd -crypt $PASS) $USER_NEW
-usermod -aG sudo $USER_NEW
-
-USER_NEW=1
-PASS=1
-
-echo "alias sudo='echo "not allowed" '" >> ~/.bashrc
 chmod 400 ~/.bashrc
-
-## delete passwords and change to reverse back
-rm -rf .git/*
-echo '' > ./bootcamp.sh
