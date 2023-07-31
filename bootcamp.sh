35db5c46bd1b09e6087f36c1f05bffb2ceb2ee37
Rihards Ratke <rihards.ratke@magebit.com>
2023-07-31 12:45:12 +0300

    Updated

--- bootcamp.sh
+++ bootcamp.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
+apt-get install -y php-fpm php-curl php-xml php-json curl
+
 # Setup PC
 snap install --classic code
+
+usermoad -a -G www-data:$USER
 
 cp ./bootcamp/nginx/server.conf /etc/nginx/sites-enabled/
 ln -s /etc/nginx/sites-enabled/server.conf /etc/nginx/sites-available/
 
 mkdir /var/www/web/
 cp -r bootcamp/server /var/www/web/
+composer install /var/www/web/server/
 
 echo "127.0.0.1 web.local" >> /etc/hosts
 service nginx restart
